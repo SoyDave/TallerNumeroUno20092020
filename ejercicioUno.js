d44a7f1f5e96b83d396f56044b02429eec8dbfed
Juan D Ortiz C <soydavex@gmail.com>
2020-09-20 17:18:28 -0500

    Ejercicio uno en sus dos formas

---
+++ ejercicioUno.js
@@ -0,0 +1,14 @@
+function calcularDistancia(coordenadax,coordenaday){
+
+    let cuadradox = Math.pow(coordenadax,2);
+    let cuadradoy = Math.pow(coordenaday,2);
+
+    let sumaCuadrados = cuadradox + cuadradoy;
+
+    let raizCuadrada = Math.sqrt(sumaCuadrados);
+
+    return (raizCuadrada);
+    
+}
+
+console.log (calcularDistancia(19, -119));
