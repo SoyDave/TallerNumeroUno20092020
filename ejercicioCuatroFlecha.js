29c690fc95d75bbbbf34e9fbad7c69cbdb320274
Juan D Ortiz C <soydavex@gmail.com>
2020-09-25 18:22:51 -0500

    Ejercicio cuatro en sus dos formas

---
+++ ejercicioCuatroFlecha.js
@@ -0,0 +1,23 @@
+let conseguirPlanos = (numeroSerie, despegar) => {
+    if (numeroSerie <= 10) {
+        let mensaje = `Tenemos el plano de la estrella numero ${numeroSerie}`;
+        despegar(null, mensaje);
+
+    } else {
+        let mensaje = "El es plano equivocado";
+        despegar(mensaje, null);
+
+    }
+}
+
+conseguirPlanos(3, (error, mensaje) => {
+
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(mensaje);
+        console.log("estamos despegando la nave");
+    }
+
+
+});
